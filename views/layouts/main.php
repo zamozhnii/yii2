239b2fe526f49269c2a9d16a4971e916d0ef756f
Igor Zamozhnii <zamozhnii@gmail.com>
2019-12-31 16:26:45 +0200

    added boostrap & header

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -1,3 +1,7 @@
+<?php
+    use \yii\bootstrap\NavBar;
+?>
+<?php $this->beginPage(); ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,9 +9,24 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Yii2</title>
+    <?php $this->head(); ?>
 </head>
 <body>
-    <?= $content ?>
-
+    <?php $this->beginBody(); ?>
+    <?php
+        NavBar::begin([
+            'brandLabel' => 'Yii2',
+            'brandUrl' => Yii::$app->homeUrl,
+            'options' => [
+                'class' => 'navbar-default'
+            ]
+        ]);
+        NavBar::end();
+    ?>
+    <div class="container">
+        <?= $content ?>
+    </div>
+    <?php $this->endBody(); ?>
 </body>
 </html>
+<?php $this->endPage(); ?>
